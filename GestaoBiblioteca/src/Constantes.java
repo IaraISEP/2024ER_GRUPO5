f75ff522e30120c33026739d03d669d2bf2d3aa2
Joaquim Silva <1241889@isep.ipp.pt>
2024-12-18 21:10:35 +0000

    update constantes

--- GestaoBiblioteca/src/Constantes.java
+++ GestaoBiblioteca/src/Constantes.java
@@ -3,10 +3,21 @@ public class Constantes {
     // Enum para Categorias
     // TODO : Servem de placeholder. A definir as Categorias
     public enum Categoria {
-        LIVRO ,
-        REVISTA,
-        JORNAL,
-        DVD
+        LIVRO(1),
+        REVISTA(2),
+        JORNAL(3);
+
+        private final int categoria;
+
+        // Construtor do enum
+        Categoria(int categoria) {
+            this.categoria = categoria;
+        }
+
+        // Método para obter o valor
+        public int getCategoria() {
+            return categoria;
+        }
     }
 
     // Enum for Estado do Emprestimo
@@ -16,5 +27,10 @@ public class Constantes {
         EMPRESTADO,
         ATRASADO,
         RESERVADO
+    }
+
+    public enum Genero {
+        MASCULINO,
+        FEMININO
     }
 }
