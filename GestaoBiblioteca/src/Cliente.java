74ad8693816e881bd843898a554dec0754d51b2a
Rui Maia <1232327@isep.ipp.pt>
2024-12-20 06:54:41 +0000

    Add validations createCliente

--- GestaoBiblioteca/src/Cliente.java
+++ GestaoBiblioteca/src/Cliente.java
@@ -56,9 +56,7 @@ public class Cliente {
         return nif;
     }
 
-    public void setNif(int nif) {
-        this.nif = nif;
-    }
+    public void setNif(int nif) { this.nif = nif; }
 
     public int getContacto() {
         return contacto;
@@ -68,22 +66,25 @@ public class Cliente {
         this.contacto = contacto;
     }
 
-    public void createCliente(){
-        int val = 0;
 
+    public void createCliente(){
+
+        int val = 0;
         System.out.print("\nPor favor, insira o Id do Cliente: ");
-        setId(input.nextInt());
+        setId(validarInteiro());
         input.nextLine();
         do{
             System.out.print("\nPor favor, insira o nif do Cliente: ");
-            val = input.nextInt();
-            input.nextLine();
+            val = validarInteiro();
             if (Integer.toString(val).length() != 9) {
-                System.out.println("Contacto Errado"); }
-        }while (Integer.toString(val).length() != 9);
+                System.out.println("NIF Invalido ( 123456789 )"); }
+        }
+        while (Integer.toString(val).length() > 9 || Integer.toString(val).length() < 9);
         setNif(val);
+        input.nextLine();
         System.out.print("\nPor favor, insira o nome do Cliente: ");
         setNome(input.nextLine());
+
         while (status){
         System.out.print("\nPor favor, insira o Genero do Cliente (1-M | 2-F): ");
         opcao = input.nextInt();
@@ -104,11 +105,11 @@ public class Cliente {
         }
         do{
             System.out.print("\nPor favor, insira o Contacto do Cliente: ");
-            val = input.nextInt();
+            val = validarInteiro();
             input.nextLine();
             if (Integer.toString(val).length() != 9) {
-                System.out.println("NIF Errado"); }
-        }while (Integer.toString(val).length() != 9);
+                System.out.println("Contacto invalido! ( 123456789 )"); }
+        }while (Integer.toString(val).length() > 9 || Integer.toString(val).length() < 9);
         setContacto(val);
 
         try {
@@ -117,6 +118,21 @@ public class Cliente {
             System.err.println("Erro ao criar Cliente" + e.getMessage());
         }
 
+    }
+
+    public int validarInteiro(){
+        boolean isInt = false;
+        int isIntVal = 0;
+        while(!isInt){
+            try {
+                isIntVal = input.nextInt();
+                isInt = true;
+            } catch (Exception e) {
+                System.out.println("Por favor, insira um número inteiro.");
+                input.nextLine();
+            }
+        }
+        return isIntVal;
     }
 
 }
