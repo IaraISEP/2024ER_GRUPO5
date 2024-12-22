c5f9e7d9feb5f6e99cc3dc983d8f6aa7f876e236
Pedro Castro <pedrocastro2003331@gmail.com>
2024-12-22 03:09:07 +0000

    Alteração da classe menu, main mais limpo, implementada uma nova classe de criação de menus.

--- GestaoBiblioteca/src/Cliente.java
+++ GestaoBiblioteca/src/Cliente.java
@@ -12,14 +12,6 @@ public class Cliente {
     private int opcao;
     private boolean status = true;
 
-    private Scanner input = new Scanner(System.in);
-
-    private tratamentoDados dados = new tratamentoDados();
-
-    public Cliente() {
-
-    }
-
     public Cliente(int id, String nome, String genero, int nif, int contacto) {
         this.id = id;
         this.nome = nome;
@@ -67,55 +59,6 @@ public class Cliente {
     }
 
 
-    public void createCliente(){
-
-        int val = 0;
-        System.out.print("\nPor favor, insira o Id do Cliente: ");
-        setId(dados.validarInteiro());
-        do{
-            System.out.print("\nPor favor, insira o nif do Cliente: ");
-            val = dados.validarInteiro();
-            if (Integer.toString(val).length() != 9) {
-                System.out.println("NIF Invalido ( 123456789 )"); }
-        }
-        while (Integer.toString(val).length() > 9 || Integer.toString(val).length() < 9);
-        setNif(val);
-        System.out.print("\nPor favor, insira o nome do Cliente: ");
-        setNome(input.nextLine());
-
-        while (status){
-        System.out.print("\nPor favor, insira o Genero do Cliente (1-M | 2-F): ");
-        opcao = input.nextInt();
-            switch (opcao){
-                case 1:
-                    setGenero("Masculino");
-                    status = false;
-                    break;
-                case 2:
-                    setGenero("Feminino");
-                    status = false;
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
-                    status = true;
-                    break;
-            }
-        }
-        do{
-            System.out.print("\nPor favor, insira o Contacto do Cliente: ");
-            val = dados.validarInteiro();
-            if (Integer.toString(val).length() != 9) {
-                System.out.println("Contacto invalido! ( 123456789 )"); }
-        }while (Integer.toString(val).length() > 9 || Integer.toString(val).length() < 9);
-        setContacto(val);
-
-        try {
-            dados.printFileCsv("clientes.csv", getId(), getNome(), getGenero(), getNif(), getContacto());
-        } catch (IOException e) {
-            System.err.println("Erro ao criar Cliente" + e.getMessage());
-        }
-
-    }
 
 
 
--- GestaoBiblioteca/src/tratamentoDados.java
+++ GestaoBiblioteca/src/tratamentoDados.java
@@ -7,28 +7,68 @@ import java.util.Scanner;
 
 public class tratamentoDados {
 
-    private Scanner input = new Scanner(System.in);
-
-    public tratamentoDados() {
-
+    private static Scanner input = new Scanner(System.in);
+    public static void criarCliente() {
+        int nif, id, contacto, opcao;
+        String nome, genero="";
+        boolean flag;
+        System.out.print("\nPor favor, insira o Id do Cliente: ");
+        id = validarInteiro();
+        do {
+            System.out.print("\nPor favor, insira o Contribuinte do Cliente: ");
+            nif = validarInteiro();
+            flag=validarTamanho(String.valueOf(nif),9);
+            if(!flag)
+                System.out.print("Contribuinte Inválido! ex: 123456789: ");
+        }while (!flag);
+        do {
+            System.out.print("\nPor favor, insira o Contacto do Cliente: ");
+            contacto = validarInteiro();
+            flag=validarTamanho(String.valueOf(contacto),9);
+            if(!flag)
+                System.out.print("Contribuinte Inválido! ex: 123456789: ");
+        }while (!flag);
+        System.out.print("\nPor favor, insira o nome do Cliente: ");
+        nome=input.next();
+        do{
+            System.out.print("\nPor favor, insira o Genero do Cliente (1-M | 2-F): ");
+            opcao = validarInteiro();
+            switch (opcao){
+                case 1:
+                    genero="Masculino";
+                    break;
+                case 2:
+                    genero="Feminino";
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }while(opcao<1||opcao>2);
+        try {
+            printFileCsv("clientes.csv", id, nome, genero, nif, contacto);
+        } catch (IOException e) {
+            System.err.println("Erro ao criar Cliente" + e.getMessage());
+        }
     }
-    public int validarInteiro(){
+    public static int validarInteiro(){
         boolean isInt = false;
-        int isIntVal = 0;
+        int valor = 0;
         while(!isInt){
             try {
-                isIntVal = input.nextInt();
+                valor = input.nextInt();
                 isInt = true;
             } catch (Exception e) {
-                System.out.println("Por favor, insira um número inteiro.");
+                System.out.print("Por favor, insira um número inteiro:");
                 input.nextLine();
             }
         }
-        return isIntVal;
+        return valor;
     }
-
-    public void printFileCsv(String ficheiro, int Id, String nome, String genero, int nif, int contacto) throws IOException {
-
+    public static boolean validarTamanho(String valor, int tamanho){
+        return valor.length()==tamanho;
+    }
+    public static void printFileCsv(String ficheiro, int Id, String nome, String genero, int nif, int contacto) throws IOException {
         FileWriter fw = new FileWriter(ficheiro, true);
         fw.append(Integer.toString(Id));
         fw.append(";");
@@ -44,8 +84,7 @@ public class tratamentoDados {
         fw.close();
     }
 
-    public void lerFicehiro(String ficheiro){
-
+    public static void lerFicehiro(String ficheiro){
         String arquivo = ficheiro;
         BufferedReader readFile = null;
         String linha = null;
