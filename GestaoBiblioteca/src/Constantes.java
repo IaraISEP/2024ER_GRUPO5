ef95db6d37945e5e01facc080dbf6f747b85b3b8
Joaquim Silva <1241889@isep.ipp.pt>
2025-01-02 22:44:34 +0000

    Updated Constantes

--- GestaoBiblioteca/src/Constantes.java
+++ GestaoBiblioteca/src/Constantes.java
@@ -1,29 +1,93 @@
 
 public class Constantes {
 
-    public enum Categoria {
-        LIVRO(1),
-        REVISTA(2),
-        JORNAL(3);
+    public enum CategoriaLivro {
+        ARTE(1),
+        BANDA_DESENHADA(2),
+        CIENCIAS(3),
+        ECONOMIA(4),
+        ENGENHARIA(5),
+        GESTAO(6),
+        HISTORIA(7),
+        INFORMATICA(8),
+        POLITICA(9),
+        SAUDE(10),
+        OUTROS(11);
 
-        private final int categoria;
+        private final int categoriaLivro;
 
-        Categoria(int categoria) {
-            this.categoria = categoria;
+        CategoriaLivro(int categoria) {
+            this.categoriaLivro = categoria;
         }
 
-        public int getCategoria() {
-            return categoria;
+        public int getCategoriaLivro() {
+            return categoriaLivro;
         }
     }
 
-    // Enum for Estado do Emprestimo
-    // TODO : Servem de placeholder. A definir os Estados
+    public enum CategoriaJornal {
+        NOTICIAS(1),
+        POLITICA(2),
+        NEGOCIOS(3),
+        DESPORTO(4),
+        CULTURA(5),
+        TECNOLOGIA(6),
+        SAUDE(7),
+        OPINIAO(8),
+        OUTROS(9);
+
+        private final int categoriaJornal;
+
+        CategoriaJornal(int categoriaJornal) {
+            this.categoriaJornal = categoriaJornal;
+        }
+
+        public int getCategoriaJornal() {
+            return categoriaJornal;
+        }
+    }
+
+    public enum CategoriaRevista {
+        ATUALIDADE(1),
+        NOTICIAS(2),
+        ENTRETENIMENTO(3),
+        CELEBRIDADES(4),
+        MODA(5),
+        LIFESTYLE(6),
+        SAUDE(7),
+        TECNOLOGIA(8),
+        AUTOMOVEIS(9),
+        DESPORTOS(10),
+        NEGOCIOS(10),
+        VIAGENS(10),
+        OUTROS(11);
+
+        private final int categoriaRevista;
+
+        CategoriaRevista(int categoriaRevista) {
+            this.categoriaRevista = categoriaRevista;
+        }
+
+        public int getCategoriaRevista() {
+            return categoriaRevista;
+        }
+    }
+
     public enum EstadoEmprestimo {
-        DISPONIVEL,
-        EMPRESTADO,
-        ATRASADO,
-        RESERVADO
+        DISPONIVEL(1),
+        EMPRESTADO(2),
+        ATRASADO(3),
+        RESERVADO(4);
+
+        private final int estadoEmprestimo;
+
+        EstadoEmprestimo(int estadoEmprestimo) {
+            this.estadoEmprestimo = estadoEmprestimo;
+        }
+
+        public int getEstadoEmprestimo() {
+            return estadoEmprestimo;
+        }
     }
 
     public enum Genero {
