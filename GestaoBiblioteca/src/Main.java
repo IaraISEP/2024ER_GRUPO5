10397d23432fc855ae18ef500049c50d60ff6192
PeterOliveiraIsep <1241622@isep.ipp.pt>
2025-01-07 21:53:29 +0000

    resstruturação do main.java

--- GestaoBiblioteca/src/Main.java
+++ GestaoBiblioteca/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
-/** Representa Class principal
+/**
+ * Classe principal da aplicação de gestão da biblioteca.
  * @author ER_GRUPO_5
  * @since 2024
  */
@@ -9,16 +10,20 @@ public class Main {
      * Método principal que inicia a aplicação de gestão da biblioteca.
      *
      * @param args Argumentos da linha de comando.
-     * @throws IOException Se ocorrer um erro de I/O durante a leitura dos ficheiros.
      */
-    public static void main(String[] args) throws IOException {
-        TratamentoDados.criarSistemaFicheiros();
-        TratamentoDados.lerFicheiroCsvClientes(Constantes.Path.CLIENTE.getValue());
-        TratamentoDados.lerFicheiroCsvLivros(Constantes.Path.LIVRO.getValue());
-        TratamentoDados.lerFicheiroCsvJornaisRevistas(Constantes.Path.JORNAL.getValue(), Constantes.TipoItem.JORNAL);
-        TratamentoDados.lerFicheiroCsvJornaisRevistas(Constantes.Path.REVISTA.getValue(), Constantes.TipoItem.REVISTA);
-        TratamentoDados.lerFicheiroCsvReservas(Constantes.Path.RESERVA.getValue());
-        TratamentoDados.lerFicheiroCsvReservasLinha(Constantes.Path.RESERVALINHA.getValue());
-        CriarMenu.menuPrincipal();
+    public static void main(String[] args) {
+        try {
+            TratamentoDados.criarSistemaFicheiros();
+            TratamentoDados.lerFicheiroCsvClientes(Constantes.Path.CLIENTE.getValue());
+            TratamentoDados.lerFicheiroCsvLivros(Constantes.Path.LIVRO.getValue());
+            TratamentoDados.lerFicheiroCsvJornaisRevistas(Constantes.Path.JORNAL.getValue(), Constantes.TipoItem.JORNAL);
+            TratamentoDados.lerFicheiroCsvJornaisRevistas(Constantes.Path.REVISTA.getValue(), Constantes.TipoItem.REVISTA);
+            TratamentoDados.lerFicheiroCsvReservas(Constantes.Path.RESERVA.getValue());
+            TratamentoDados.lerFicheiroCsvReservasLinha(Constantes.Path.RESERVALINHA.getValue());
+            CriarMenu.menuPrincipal();
+        } catch (IOException e) {
+            System.err.println("Erro durante a execução: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
